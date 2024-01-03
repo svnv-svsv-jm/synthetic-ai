c25dc6256048e166d30dbe2ed28cab2377c9b010
svnv-svsv-jm <janmail1990@gmail.com>
2024-01-03 17:39:54 +0100

    --cov-fail-under 98

--- scripts/pytest.sh
+++ scripts/pytest.sh
@@ -9,4 +9,4 @@ mkdir -p logs
 
 $PYTHON -m pytest -x --testmon --nbmake --overwrite "./examples"
 $PYTHON -m mypy test
-$PYTHON -m pytest -x --testmon --pylint
+$PYTHON -m pytest -x --testmon --pylint --cov-fail-under 98
